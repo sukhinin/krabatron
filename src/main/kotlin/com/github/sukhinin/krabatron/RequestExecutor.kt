a3f63f71f3e6f980ddc1b7e5630672602df6190b
Boris Sukhinin <boris.sukhinin@corp.mail.ru>
2021-05-12 19:21:04 +0300

    add convenience extension functions

--- src/main/kotlin/com/github/sukhinin/krabatron/RequestExecutor.kt
+++ src/main/kotlin/com/github/sukhinin/krabatron/RequestExecutor.kt
@@ -1,5 +1,21 @@
 package com.github.sukhinin.krabatron
 
+import kotlinx.coroutines.runBlocking
+
 interface RequestExecutor<T> {
     suspend fun get(request: Request): T
+}
+
+suspend fun <T> RequestExecutor<T>.get(url: String): T {
+    return get(Request(url))
+}
+
+fun <T> RequestExecutor<T>.getBlocking(request: Request): T {
+    val executor = this
+    return runBlocking { executor.get(request) }
+}
+
+fun <T> RequestExecutor<T>.getBlocking(url: String): T {
+    val executor = this
+    return runBlocking { executor.get(url) }
 }
